557de87875bfb35edfafd3606d0c6b717b55e13a
mbusry <mbusry@fullsail.edu>
2013-03-20 23:17:03 -0400

    Week 3 RC1

---
+++ week3/gold/js/gotoSearch.js
@@ -0,0 +1,9 @@
+function getID(x) {
+	var elementID = document.getElementById(x);
+	return elementID;
+};
+
+// **************************************************************
+// Search added for project II
+var search = getID('search-basic');
+search.addEventListener("click", search.html);
