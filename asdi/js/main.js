a57560843e7f3f37c4af038c0a5793ec56503f32
Michael Usry <mbusry@fullsail.edu>
2013-05-06 21:46:07 -0400

    asdi #1

---
+++ asdi/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Michael Usry, ASDI 1305
+ */
+
+
+    // Global variables
+
+
+// getElementById
+    function getID(x){
+        var elementID = document.getElementById(x);
+        return elementID;
+    };
+    
+
+    
