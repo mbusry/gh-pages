a99ec5b780f4f68c0a0f8f12df071e0592e2b44a
Michael Usry <mbusry@fullsail.edu>
2013-05-08 22:56:01 -0400

    Week 1

--- asdi/js/main.js
+++ asdi/js/main.js
@@ -2,6 +2,27 @@
  * Michael Usry, ASDI 1305
  */
 
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+
+$('#addAttendant').on('pageinit', function(){
+
+		var myForm = $('#addForm');
+		    myForm.validate({
+			invalidHandler: function(form, validator) {
+			},
+			submitHandler: function() {
+		var data = myForm.serializeArray();
+			saveData(data);
+		}
+	});
+	
+	//any other code needed for addItem page goes here
+	
+});
+
+
 
     // Global variables
 
