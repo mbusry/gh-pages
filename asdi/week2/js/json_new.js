723ecf8ce9ad5135926a57c50852ca0d55912a51
Michael Usry <mbusry@fullsail.edu>
2013-05-16 00:26:39 -0400

    v1.12

---
+++ asdi/week2/js/json_new.js
@@ -0,0 +1,10 @@
+var json = {
+	"item1" : {
+        "className"      : ["Assigned to:", "Mom"],
+        "currentDate"    : ["eMail:", "test@me.com"],
+        "fname"          : ["Shop:", "Ingles"],
+        "lname"          : ["When:", "2013-02-25"],
+        "phoneNumber"    : ["Grocery Item:", "Meat"]
+        
+    }
+};    
