b7cfb6c73cbf3b0a4a9f7f1e5b66d318527e8e13
Michael Usry <mbusry@fullsail.edu>
2013-05-16 21:23:50 -0400

    W2v1

---
+++ asdi/week2-2/xhr/json.js
@@ -0,0 +1,32 @@
+var json = {
+	"people" : [
+	{
+		"className"   	: ["prek:", "teen"],
+		"currentDate" 	: ["currentDate", "05/19/13"],
+		"fname" 		: ["fname", "Jake"],
+		"lname" 		: ["lname", "Carter"],
+		"phoneNumber" 	: ["phonenumber", "7709801287"]
+	},
+	{
+		"className" 	: ["prek:", "adult"],
+		"currentDate" 	: ["currentDate", "05/19/13"],
+		"fname" 		: ["fname", "John"],
+		"lname" 		: ["lname", "Carter"],
+		"phoneNumber" 	: ["phonenumber", "7709807801"]
+	},
+	{
+		"className" 	: ["prek:", "prek"],
+		"currentDate" 	: ["currentDate", "05/19/13"],
+		"fname" 		: ["fname", "Blake"],
+		"lname" 		: ["lname", "Carter"],
+		"phoneNumber" 	: ["phonenumber", "7709871358"]
+	},
+	{
+		"className" 	: ["prek:", "elementary"],
+		"currentDate" 	: ["currentDate", "05/22/13"],
+		"fname" 		: ["fname", "Will"],
+		"lname" 		: ["lname", "Isley"],
+		"phoneNumber" 	: ["phonenumber", "7705894591"]
+	}
+]
+}
