f412d3cbdad512422892f40db716cf268f6107d5
Michael Usry <mbusry@fullsail.edu>
2013-07-11 20:32:49 -0400

    v1.1

---
+++ VFA/ios/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * @author Michael Usry VFA 1307
+ */
+
+    $('#listPeople').on('click', function() {
+// extra code goes here
+		testClass;
+    });
+
+var testClass = function(){
+	console.log("Going to the page works!");
+};
+
